66f5b438226b56a53620ecd9bc5e599bb203aab6
chenshifeng <1162704960@qq.com>
2021-06-06 10:35:42 +0800

    增加辅助函数

--- sftest.py
+++ sftest.py
@@ -7,19 +7,13 @@
 @功能：存放一些公共方法
 """
 
-import datetime
+from interface.debugtalk import *
 
 
-def get_date(offsetDay=0):
-    today = datetime.datetime.now()
-    # 计算偏移量
-    offset = datetime.timedelta(days=+offsetDay)
-    # 获取想要的日期的时间
-    re_date = (today + offset).strftime('%Y-%m-%d %H:%M:%S')
-    return re_date
+
 
 
 
 if __name__ == '__main__':
-    print(get_date(offsetDay=1)[0:10])
-    print(type(str(get_date(offsetDay=1)[0:10])))
+    print(create_ident_generator())
+    print(create_phone_generator())
